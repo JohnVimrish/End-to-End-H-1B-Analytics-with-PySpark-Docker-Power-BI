dd069673c0119cea2f799204631756c3b5f86a8d
john_vimrish <john.vimri@gmail.com>
2025-02-04 22:03:52 +0000

    Removal of Un-Nessscary Files

--- code/database/postgresconnector.py
+++ code/database/postgresconnector.py
@@ -1,31 +1,55 @@
 import sys
-from itertools import islice
 from datetime import datetime as dt
-from util.fusionloggingutility import LoggingUtil
-# from psycopg2 import extras as ex
 from psycopg import Error, DatabaseError as db_error
-from psycopg import Transaction
-from database.postgresconnectionpool import PostgresConnectionPool
-from transform.transformdata import Transform_Data
-import numpy as np
-# from psycopg2.extensions import register_adapter, AsIs
+
+
 
 
 class PostgresConnection():
 
-    def __init__(self,
-                 connection_pool: PostgresConnectionPool,
-                 log_file_handler,
-                 log_obj: LoggingUtil,
-                 log_level,
-                 table_name,
-                 db_type):
-        self.connection_pool = connection_pool
-        logger_name = db_type + '-' + table_name
-        self.pg_logger = log_obj.setup_logger(logger_name, log_level)
-        log_obj.link_logger_filehandler(self.pg_logger, log_file_handler)
-        # register_adapter(np.int64,AsIs)
-        self.write_row_count = 0
+    def __init__ (self, config_connect_vars: dict) :
+
+            user_id = config_connect_vars[cv.user_id]
+            password = config_connect_vars[cv.password]
+            port = config_connect_vars[cv.port]
+            host = config_connect_vars[cv.host]
+            min_connections = config_connect_vars[cv.min_connection_pools]
+            max_connections = config_connect_vars[cv.max_connection_pools]
+            database = config_connect_vars[cv.database]
+            tns = config_connect_vars[cv.tns]
+            db_type = config_connect_vars[cv.database_type]
+            dsn = host+':'+port+'/' + database
+            self.db_connector_name = db_connector_name
+
+            database_config = f"user={user_id} password={password} host={host} port={port} dbname={database} keepalives={1} keepalives_idle={100} keepalives_interval={100} keepalives_count={5} tcp_user_timeout={100000}"
+            try:
+                # self.connection_pool = ConnectionPool(
+                #     maxconn=max_connections,
+                #     user=user_id,
+                #     password=password,
+                #     host=host,
+                #     port=port,
+                #     database=database,
+                #     keepalives=1,
+                #     keepalives_idle=100,
+                #     keepalives_interval=100,
+                #     keepalives_count=5,
+                #     tcp_user_timeout=100000)
+                self.connection_pool =ConnectionPool( conninfo =database_config,min_size=10,max_size=10 )
+                
+                str1 = f'{db_connector_name} Connector with  user_name:{user_id}, password : xxxx, '
+                str2 = f'host :{host}, port :{port}, tns :{tns},database : {database}, '
+                str3 = f'min_num of connections :{min_connections},'
+                str4 = f'max_num of connections :{max_connections}'
+                str5 = f'db_type :{db_type} , dsn:{dsn} has connected !'
+
+                self.pg_cp_logger.info(str1 + str2 + str3 + str4 + str5)
+
+            except (OperationalError, Exception, Error) as err:
+                # passing exception to function
+                self.pg_cp_logger.critical('{0} Connector  has failed to connect with error :{1}'.format(
+                    db_connector_name, err), exc_info=True)
+
 
     def acquire_connection(self):
         try:
@@ -45,105 +69,6 @@ class PostgresConnection():
         self.pg_logger.error(log_statement.format(
            pg_exception_result [0]), exc_info=True)
         self.cursor_object.close()
-
-    def execute_copy_activity (self,copy_sql:str ,csv_input_location :str,error_message, write_batches) :
-        try:
-            if self.new_connection == None:
-                self.acquire_connection()
-            with open(csv_input_location, 'r') as f:
-              with self.cursor_object.copy(copy_sql) as copy_execute :
-                while True:
-                    data = f.read(write_batches)
-                    if not data:
-                        break
-                    copy_execute.write(data)
-            self.new_connection.commit
-        except (Error, db_error) as error:
-            self.log_and_close(error_message, error)
-
-
-    def split_into_batches(self, arr_range, arr_size):
-        arr_range     = iter(arr_range)
-        return list(iter(lambda: tuple(islice(arr_range, arr_size)), ()))
-
-
-    def __execute_pipeline (self,write_query,write_inputs) :
-        with Transaction(self.new_connection) as tx:
-            with self.new_connection.pipeline():
-                self.cursor_object.executemany(write_query,write_inputs)
-               
-    def upsert_deleted_data  (  self, 
-                                write_inputs,
-                                write_query) :
-
-
-        self.pg_logger.debug(f'Deleted Records write starts at {dt.now()}')
-
-        
-        # ex.execute_values(  self.cursor_object, 
-        #                     write_query,
-        #                     write_inputs,
-        #                     template=None,
-        #                     fetch=False)
-        self.__execute_pipeline(write_inputs,write_query)
-        
-        self.pg_logger.info(f'Number of Rows written :{self.get_curr_exec_rowcount()}')
-        self.pg_logger.debug(f'Deleted Records write  ends at {dt.now()}')
-
-    def upsert_data(self, 
-                    write_inputs,
-                    write_query,
-                    write_bacthes_size,
-                    src_batch_num):
-
-        self.pg_logger.debug(f'Source Batch Number: {src_batch_num} write starts at {dt.now()}')
-
-        self.__execute_pipeline(write_inputs,write_query)
-        # ex.execute_values(  self.cursor_object, 
-        #                     write_query,
-        #                     write_inputs,
-        #                     template=None,
-        #                     page_size=write_bacthes_size,
-        #                     fetch=False)
-        written_records_count = self.get_curr_exec_rowcount()
-        self.write_row_count += written_records_count
-        
-        self.pg_logger.info(f'Number of Rows written :{written_records_count}')
-        self.pg_logger.debug(f'Source Batch Number: {src_batch_num} write ends at {dt.now()}')
-
-    def upsert_data_in_batches(self,
-                                input_data,
-                                write_query,
-                                batch_size,
-                                src_batch_num):
-        try:
-            
-            for data in self.split_into_batches( input_data, batch_size):
-                batch_data = list(data)
-                try:
-                    self.upsert_data(batch_data,
-                                    write_query,
-                                    batch_size,
-                                    src_batch_num)
-              
-                except (Exception, db_error) as err:
-                    if str(err) == 'A string literal cannot contain NUL (0x00) characters.':
-                        try:
-                            fixed_data = Transform_Data.remove_ascii_zero_char(batch_data)
-                            self.upsert_data(   fixed_data,
-                                                write_query,
-                                                batch_size,
-                                                src_batch_num)
-                        except (Exception, db_error) as error:
-                            self.log_and_close(
-                                'Error  has occured while  executing the source results on  Target DB {}.', err)
-                    else:
-                        self.log_and_close(
-                            'Error  has occured while  executing the source results on  Target DB {}.', err)
-        except (Exception) as error:
-            self.pg_logger.error(
-                'Error occured in Method Execute_batch_results :{} .'.format(error), exc_info=True)
-            self.cursor_object.close()
 
     def execute_sql(self, sql, error_message,query_argments = None):
         try:
@@ -178,12 +103,8 @@ class PostgresConnection():
                 'Unable to Close Connection , Error :{} .', error)
 
     def log_pgdb_exception(self, err_msg_obj):
-        # get details about the exception
         err_type, err_obj, traceback = sys.exc_info()
-        # get the line number when exception occured
         line_n = traceback.tb_lineno
-        # print the connect() error
-
         str1 = f' npsycopg2 ERROR:{err_msg_obj} on line number:{line_n}\npsycopg2 traceback:{traceback}'
         str2 = f' -- type:{err_type}\npgerror:{err_msg_obj.pgerror}\n pgcode: {err_msg_obj.pgcode}'
         return str1 + str2,err_msg_obj.pgerror
--- code/root/main.py
+++ code/root/main.py
@@ -1,144 +1,113 @@
-from core.jsongroupexecution import JSONGroupExecution
-from util.fusionloggingutility import LoggingUtil
-from util.fileutility import FileUtility
-from jsoncustom.jsonvalueextract import JsonValueExtractor
-from jsoncustom.jsontagncommonvariables import JsonTagAndCommonVariables
-from core.recoverfailedexecutions import ExecutionRecovery
-from importcsv.csvimport import CSVImportUsingDTF
-from database.dbinitialisers import DatabaseInitialiser
-from util.emailsender import EMailSummaryLogAttachments as mail_attachments
+from datetime import datetime as dt
 import sys
-from datetime import datetime
+import traceback
+from os.path import dirname as directoryname, realpath
+from database.dbinitialisers import DatabaseInitialiser
+from jsonmanipulations.jsontagncommonvariables import JsonTagAndCommonVariables
+from jsonmanipulations.jsonvalueextract import JsonValueExtractor
+from root.commonvariables import CommonVariables
+from jsonmanipulations.configparametervalue import ConfigParametersValue
 
 
-def main_thread():
+def main_function():
 
-    config_json_filepath = sys.argv[1]
-    connection_json_filepath = sys.argv[2]
-
-    program_timer = dict()
-
-    config_tags = JsonTagAndCommonVariables()
-    config_vars = config_tags
-
-    main_log_name = config_vars.main_log_name
-
-    program_timer[config_vars.load_start_time] = datetime.now()
-
-# Initialize Main configuration without logger
-    config_obj = JsonValueExtractor(config_json_filepath, main_log_name)
-
-## Initialize all the variables
-    config_vars.log_levels = config_obj.json_val(config_vars.log_level_config)
-    log_levels = config_vars.log_levels
-    main_log_level = log_levels[config_vars.main_log_level]
-    main_log_filepath = config_vars.main_log_file_path
-
-    json_log_level = log_levels[config_vars.json_log_level]
-    log_file_path = config_obj.json_val(config_vars.log_file_path)
-    log_dir_name_format = config_obj.json_val(config_vars.log_folder_name_format)
-
-## Initialize Logger
-    main_log_obj = LoggingUtil(log_file_path)
-    FileUtility.create_directory(datetime.now().strftime(log_dir_name_format))
-    main_logger = main_log_obj.setup_logger(main_log_name, main_log_level)
-    main_logger_file_handler = main_log_obj.create_log_file(main_log_filepath)
-
-    main_log_obj.link_logger_filehandler(main_logger, main_logger_file_handler)
-
-## Assign the logger to Main Configuration
-    config_obj.reinitialise_logger_object( main_log_obj, 
-                                           config_vars.main_config,
-                                           main_logger_file_handler, 
-                                           json_log_level)
-
-# email class initialiser 
-    load_start_mail_subject      = config_obj.json_val(config_vars.email_subject).format(f'has Started ')
-
-    # email_obj      =  mail_attachments(
-    #                     config_obj.json_val(config_vars.smtp_server_host),
-    #                     config_obj.json_val(config_vars.smtp_server_port),
-    #                     config_obj.json_val(config_vars.email_sender_id),
-    #                     config_obj.json_val(config_vars.email_receiver_id),
-    #                     config_obj.json_val(config_vars.email_subject)                        
-    #                     )
-
-    # email_obj.send_load_start_viamail (load_start_mail_subject)
-## Initialize  variables  after logger is initialised
-    ora_instant_client_dir = config_obj.json_val(config_vars.ora_instant_client_dir)
-
-    csv_import_params = config_obj.json_val(config_vars.csv_import_parameters)
-    csv_import_filepath = csv_import_params[config_vars.csv_import_file_info_path]
-    csv_import_flag = csv_import_params[config_vars.csv_import_flg]
-
-    prev_run_success_file = config_obj.json_val(config_vars.pre_sucessfull_run_tables_list)
-    prev_run_full_file = config_obj.json_val(config_vars.pre_execution_complete_tbl_list)
-    recovery_flag_config_file = config_obj.json_val(config_vars.recover_failed_exec_flg)
-
-    curr_tbl_dict =  config_obj.json_val(config_vars.table_information_for_processing)
-    load_type =  config_obj.json_val(config_vars.load_type)
-    max_parallel_num = config_obj.json_val(config_vars.max_parallel_num)   
-
-## Initialize Connection Configuration with logger
-    conn_config_obj = JsonValueExtractor(   connection_json_filepath, 
-                                            config_vars.connection_param,
-                                            main_log_obj,
-                                            main_logger_file_handler,
-                                            json_log_level)
-
-    connectors_dict = conn_config_obj.json_val(config_vars.connectors)
-
-    main_logger.info('ETL Start Time : {} .'.format(datetime.now()))
-
-    main_logger.info("Connection Pool Establishment starts!")
-
-    db_obj = DatabaseInitialiser(   connectors_dict,
-                                    main_log_obj,
-                                    main_logger_file_handler, 
-                                    config_vars, 
-                                    ora_instant_client_dir,
-                                    config_obj.json_val(config_vars.connection_retry_count))
-
-    main_logger.info("Connection Pool Establishment ends!")
-
-    if csv_import_flag == 'Y':
-        csv_import_obj = CSVImportUsingDTF(csv_import_filepath,
-                                    db_obj,
-                                    main_log_obj,
-                                    config_vars)
-        csv_import_obj.load_csv_files_to_db()
-    
-    recover_prev_exec_obj = ExecutionRecovery ( prev_run_success_file,
-                                                prev_run_full_file,
-                                                recovery_flag_config_file)
-
-    exec_obj = JSONGroupExecution(  config_vars, 
-                                    curr_tbl_dict, 
-                                    main_log_obj,
-                                    main_logger_file_handler,
-                                    db_obj)
-
-    exec_obj.process_groups(recover_prev_exec_obj,config_obj.json_val(config_vars.table_config_file_path))
-
-    table_log_summary, group_log_summary = exec_obj.process_tables(load_type, max_parallel_num)
-
-    program_timer[config_vars.load_end_time] = datetime.now()
-
-    current_run_log_directory   =  main_log_obj.summary_log_file ( config_vars,
-                                    table_log_summary, 
-                                    group_log_summary, 
-                                    program_timer,
-                                    config_obj.json_val(config_vars.common_summary_log_folder_name)
-                                    )
+    config_json_object                             = JsonValueExtractor(sys.argv[1])
+    connection_json_object                         = JsonValueExtractor(sys.argv[2])
+    storage_connection_object                      = JsonValueExtractor(sys.argv[3])
 
 
+    ConfigParametersValue(config_json_object, connection_json_object, storage_connection_object, 
+                          fusion_jobs_object,
+                          log_level_object,
+                          table_groups_object,
+                          load_missing_viewobjects_json_object,
+                          bipublisher_individual_report_configuration,
+                          import_etl_dependent_csv_configuration,
+                          job_scheduling_parameter_inputs_conf)
+                          
 
-    # email_obj.send_load_status_viamail(group_log_summary,
-    #                                    config_obj.json_val(config_vars.email_attachments),
-    #                                    current_run_log_directory)
+    try:
+        main_logging_obj = LoggingUtil(
+            ConfigParametersValue.log_base_directory
+            )
+        FusionMainLogger = main_logging_obj.setup_logger(
+            CommonVariables.main_config, ConfigParametersValue.fusion_main_log_level)
+        processid  = int (dt.now().strftime(ConfigParametersValue.main_log_file_name_format))
+        main_log_file = f'{processid}.log'
+        main_logfile = main_logging_obj.create_log_file(main_log_file)
+        main_logging_obj.link_logger_filehandler(FusionMainLogger,main_logfile)
+        start_time = dt.now()
+        FusionMainLogger.info('ETL Start Time : {} .'.format(start_time))
+        FusionMainLogger.info(f'Process ID : {processid}, shall be usefull to query logs in DB')
+        main_logging_obj.init_program_process_id(processid)
+        main_logging_obj.init_logging_queries(ConfigParametersValue.table_level_log_input_query ,
+                                              ConfigParametersValue.download_obj_level_log_input_query, 
+                                              ConfigParametersValue.overall_etl_log_insert_query,
+                                              ConfigParametersValue.download_obj_requestid_fetch_query,
+                                              ConfigParametersValue.download_obj_summary_query,
+                                              ConfigParametersValue.table_etl_requestid_fetch_query,
+                                              ConfigParametersValue.table_etl_summary_recovered_flow_query,
+                                              ConfigParametersValue.table_etl_summary_normal_flow_query,
+                                              ConfigParametersValue.overall_summary_query)
 
+        ObjJSONnCommonVariables = JsonTagAndCommonVariables ()
+        ObjJSONnCommonVariables.log_levels = ConfigParametersValue.all_packages_log_level
+        FusionMainLogger.info("Connection Pool Establishment starts!")
+        ObjDBInitialiser        = DatabaseInitialiser(ConfigParametersValue.dbconnectors,
+                                                            main_logging_obj,
+                                                            main_logfile,
+                                                            ObjJSONnCommonVariables,
+                                                            ConfigParametersValue.ora_instant_client_dir,
+                                                            ConfigParametersValue.oracle_connection_retry_count)
+        FusionMainLogger.info("Connection Pool Establishment ends!")
+        logdbconnection = ObjDBInitialiser.init_db_connection(
+                                                  ConfigParametersValue.log_db_connection_name,
+                                                  FusionMainLogger,
+                                                  main_logfile ,
+                                                  main_logging_obj,
+                                                  'Logger DB' )
+        if logdbconnection is not None : 
+
+            main_logging_obj.init_logdb_connection(logdbconnection)                                                                                       
+            config_json_object.reinitialise_logger_object(
+                main_logging_obj, CommonVariables.main_config, main_logfile, ConfigParametersValue.json_value_extc_log_level)
+            connection_json_object.reinitialise_logger_object(
+                main_logging_obj, CommonVariables.connection_param, main_logfile, ConfigParametersValue.json_value_extc_log_level)
+            storage_connection_object.reinitialise_logger_object(
+                main_logging_obj, CommonVariables.storage_conn, main_logfile, ConfigParametersValue.json_value_extc_log_level)
+            fusion_jobs_object.reinitialise_logger_object(
+                main_logging_obj, CommonVariables.fusion_job_config, main_logfile, ConfigParametersValue.json_value_extc_log_level)
+            log_level_object.reinitialise_logger_object(
+                main_logging_obj, CommonVariables.log_level_config, main_logfile, ConfigParametersValue.json_value_extc_log_level)
+            table_groups_object.reinitialise_logger_object(
+                main_logging_obj, CommonVariables.table_config, main_logfile, ConfigParametersValue.json_value_extc_log_level)
+            load_start_mail_subject = ConfigParametersValue.email_subjectline.format('has Started .')
+            # email_obj.send_load_start_viamail(load_start_mail_subject)
+            ObjDownloadProcess = DownloadProcess(main_logging_obj, main_logfile)
+            DownloadProcessStatus = ObjDownloadProcess.download_NProcess_jobs(ObjDBInitialiser,processid)
+            end_time  = dt.now()
+            FusionMainLogger.info('ETL End Time : {} .'.format(end_time))
+            main_logging_obj.log_program_etl(dict(group_name = 'Complete Load' , group_id = -20, 
+                                            status = DownloadProcessStatus , group_execution_start_time = start_time  ,
+                                            group_execution_end_time  = end_time))
+            Overall_summary_file_path, Attachments = main_logging_obj.generate_summary_log_files(ConfigParametersValue.log_summary_directory,
+                                                        ConfigParametersValue.log_etl_summary_file,
+                                                        ConfigParametersValue.log_download_summary_file,
+                                                        ConfigParametersValue.log_table_summary_file)
+            ObjDBInitialiser.release_connection(main_logging_obj.ObjLogDbConnection)
+            ObjDBInitialiser.close_connections()
+            
+    except Exception as error:
+        ex_type, ex_value, ex_traceback = sys.exc_info()
+        trace_back = traceback.extract_tb(ex_traceback)
+        stack_trace = list()
+        for trace in trace_back:
+           stack_trace.append("File : %s , Line : %d, Func.Name : %s, Message : %s" %(trace[0], trace[1], trace[2], trace[3]))
+        email_message = '\n'.join(["Exception type : %s " % ex_type.__name__,
+                             "Exception message : %s" %ex_value ,
+                             "Stack trace : %s" %stack_trace])
+        raise email_message
 
 if __name__ == "__main__":
-
-    main_thread()
-     
+    CommonVariables.etl_project_directory = directoryname(directoryname(directoryname(realpath(__file__))))
+    main_function()
